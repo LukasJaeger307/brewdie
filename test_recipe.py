6b56a816fb3d60e71bf46a1881cae8607693c99e
Lukas Jäger <lukas_jaeger@gmx.net>
2021-08-16 20:04:25 +0200

    Refactor initial test

--- test_recipe.py
+++ test_recipe.py
@@ -22,17 +22,31 @@ class TestSum(unittest.TestCase):
 
         return recipe
 
+    def is_example_recipe(self, recipe):
+        result = (recipe.name == "North Star")
+        result = result and (recipe.style == "American Pale Ale")
+        result = result and (len(recipe.rests) == 0)
+        result = result and (len(recipe.malts) == 0)
+        result = result and (recipe.boiling_minutes == 90)
+        result = result and (len(recipe.hop_dosages) == 0)
+        result = result and (recipe.litres == 4.7)
+        result = result and (recipe.sugar_gramms_per_litre == 7.4)
+        result = result and (len(recipe.additional_ingredients) == 0)
+        return result
+
+
     def test_recipe_constructor(self):
         recipe = self.create_example_recipe()
-        self.assertEqual(recipe.name, "North Star")
-        self.assertEqual(recipe.style, "American Pale Ale")
-        self.assertEqual(len(recipe.rests), 0)
-        self.assertEqual(len(recipe.malts), 0)
-        self.assertEqual(recipe.boiling_minutes, 90)
-        self.assertEqual(len(recipe.hop_dosages), 0)
-        self.assertEqual(recipe.litres, 4.7)
-        self.assertEqual(recipe.sugar_gramms_per_litre, 7.4)
-        self.assertEqual(len(recipe.additional_ingredients), 0)
+        self.assertTrue(self.is_example_recipe(recipe))
+        #self.assertEqual(recipe.name, "North Star")
+        #self.assertEqual(recipe.style, "American Pale Ale")
+        #self.assertEqual(len(recipe.rests), 0)
+        #self.assertEqual(len(recipe.malts), 0)
+        #self.assertEqual(recipe.boiling_minutes, 90)
+        #self.assertEqual(len(recipe.hop_dosages), 0)
+        #self.assertEqual(recipe.litres, 4.7)
+        #self.assertEqual(recipe.sugar_gramms_per_litre, 7.4)
+        #self.assertEqual(len(recipe.additional_ingredients), 0)
         
 
 if __name__ == '__main__':
